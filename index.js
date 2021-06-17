b951b0dcaa5b5c3c8598eb75ad28ff4afcd40d2f
Harold Struwig <harold.struwig@safetyio.com>
2021-06-17 09:43:23 +0200

    implement gcode file post [wip]

--- index.js
+++ index.js
@@ -5,11 +5,9 @@ var axios = require('axios');
 
 const ip = '127.0.0.1'
 const port = '8080'
-const filepath = path.join(__dirname, 'files', 'cyrus_test.gcode');
+const filesDir = path.join(__dirname, 'files');
 
-let filedata;
-
-run()
+uploadFileToController('cyrus_test.gcode')
     .then(() => {
         console.log('SUCCESS');
     })
@@ -17,16 +15,29 @@ run()
         console.log('FAIL', err);
     });
 
-async function run () {
-    filedata = await fsp.readFile(filepath, {encoding: 'utf8'});
-    // todo - compose uri params instead of hardcoding
-    const path = '/rr_upload?name=0%3A%2Fgcodes%2Fcyrus_test.gcode&time=2021-6-16T12%3A23%3A7&crc32=7f0d5568';
-    await postFile(filedata, path);
+async function uploadFileToController (filename) {
+    const data = await fsp.readFile(path.join(filesDir, filename), {encoding: 'utf8'});
+    const name = `0:/gcodes/${filename}`;
+    const time = (new Date()).toISOString();
+    // const crc32 = `7f0d5568`;
+    const crc32 = `7f0d5568`;
+    const params = {
+        name,
+        time,
+        crc32
+    };
+    await postFile('/rr_upload', params, data);
 }
 
-async function postFile(filedata, path) {
-    const url = `http://${ip}:${port}${path}`;
-    const result = await axios.post(url, filedata);
+async function postFile(uriPath, params, data) {
+    const url = `http://${ip}:${port}${uriPath}`;
+    const method = 'post';
+    const result = await axios({
+        method,
+        url,
+        params,
+        data
+    });
     if (result.data.err !== 0) throw new Error('error from duet: ' + result.data.err);
     return result;
 }
